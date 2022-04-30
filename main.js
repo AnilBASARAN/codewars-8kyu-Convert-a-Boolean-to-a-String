0fc902780452729ade4dfb46e3532d5b58fd9e20
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-01 01:17:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let booleanToString = (b) => {
+    if(b == true){
+      return "true"
+    }else if ( b == false){
+      return "false"
+    }
+  }
+
+
+
+/*
+8kyu Convert a Boolean to a String
+
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
